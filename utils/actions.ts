f8739ccfdb8e88459f9c4b4c421b3f5eb034f20b
Nak <nakwondersk@gmail.com>
2024-09-02 15:16:47 +0700

    change database to mongodb

--- utils/actions.ts
+++ utils/actions.ts
@@ -163,6 +163,32 @@ export const createPropertyAction = async (
   redirect("/");
 };
 
+// export const createPropertyAction = async (
+//   prevState: any,
+//   formData: FormData
+// ): Promise<{ message: string }> => {
+//   const user = await getAuthUser();
+//   try {
+//     const rawData = Object.fromEntries(formData);
+//     const file = formData.get("image") as File;
+
+//     const validatedFields = validateWithZodSchema(propertySchema, rawData);
+//     const validatedFile = validateWithZodSchema(imageSchema, { image: file });
+//     const fullPath = await uploadImage(validatedFile.image);
+
+//     await db.property.create({
+//       data: {
+//         ...validatedFields,
+//         image: fullPath,
+//         profileId: user.id,
+//       },
+//     });
+//   } catch (error) {
+//     return renderError(error);
+//   }
+//   redirect("/");
+// };
+
 export const fetchProperties = async ({
   search = "",
   category,
